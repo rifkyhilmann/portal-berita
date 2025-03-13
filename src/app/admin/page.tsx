ab90b109d1421a1e8285230e7a17e235d1023a9b
Rifky Hilman <rifkyhilmanudin965@gmail.com>
2025-03-14 00:15:56 +0700

    feat: add element card dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,6 +2,7 @@
 
 import LayoutAdmin from "@/components/template/LayoutAdmin"
 import { Breadcrumb } from "antd"
+import { MdArticle, MdFolder, MdOutlineArticle } from "react-icons/md"
 
 
 const Page = () => {
@@ -15,10 +16,32 @@ const Page = () => {
                     }
                 ]}
             />
-            <div className="grid xl:grid-cols-3 md:grid-cols-3 grid-cols-1 gap-4">
-                <div className="h-[150px] w-full bg-white shadow flex flex-col items-center p-5 rounded-lg">
-                    <div className="h-10 w-10 rounded-full bg-black flex items-center justify-center">
-                        
+            <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+                <div className="h-[150px] w-full bg-white shadow flex flex-col justify-center gap-4 items-center p-5 rounded-lg">
+                    <div className="h-12 w-12 text-white text-xl rounded-full bg-black flex items-center justify-center">
+                        <MdArticle  />
+                    </div>
+                    <div className="flex flex-col items-center  ">
+                        <h1 className="text-sm">Jumlah Published</h1>
+                        <h1 className="text-sm font-semibold">0</h1>
+                    </div>
+                </div>
+                <div className="h-[150px] w-full bg-white shadow flex flex-col justify-center gap-4 items-center p-5 rounded-lg">
+                    <div className="h-12 w-12 text-white text-xl rounded-full bg-black flex items-center justify-center">
+                        <MdArticle  />
+                    </div>
+                    <div className="flex flex-col items-center  ">
+                        <h1 className="text-sm">Jumlah Draft</h1>
+                        <h1 className="text-sm font-semibold">0</h1>
+                    </div>
+                </div>
+                <div className="h-[150px] w-full bg-white shadow flex flex-col justify-center gap-4 items-center p-5 rounded-lg">
+                    <div className="h-12 w-12 text-white text-xl rounded-full bg-black flex items-center justify-center">
+                        <MdFolder  />
+                    </div>
+                    <div className="flex flex-col items-center  ">
+                        <h1 className="text-sm">Jumlah Category</h1>
+                        <h1 className="text-sm font-semibold">0</h1>
                     </div>
                 </div>
             </div>
