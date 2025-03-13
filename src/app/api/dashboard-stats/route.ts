08bab8f1e776aba2023de1b88488829e6bcfb50c
Rifky Hilman <rifkyhilmanudin965@gmail.com>
2025-03-14 00:19:22 +0700

    feat: add api dashboard stats

---
+++ src/app/api/dashboard-stats/route.ts
@@ -0,0 +1,30 @@
+import prisma from "@/utils/prisma"
+import { createResponse } from "@/utils/response";
+
+
+export const GET = async (request : Request) => {
+    try {
+        const countPublished = await prisma.article.count({
+            where: { status: "published" }
+        });
+
+        // Hitung jumlah artikel dengan status "draft"
+        const countDraft = await prisma.article.count({
+            where: { status: "draft" }
+        });
+
+        // Hitung jumlah kategori
+        const countCategory = await prisma.category.count();
+
+        const data = {
+            countPublished,
+            countDraft,
+            countCategory
+        };
+        
+        return createResponse(200, 'success', data);
+    } catch (error) {
+        return createResponse(500, 'Internal Server Error');
+
+    }
+}
