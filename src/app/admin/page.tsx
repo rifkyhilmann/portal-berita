705b432ae91fabdf30b019265f84f3eabb2ae63d
Rifky Hilman <rifkyhilmanudin965@gmail.com>
2025-03-14 01:28:47 +0700

    feat: fix dashboard admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,6 +3,7 @@
 import LayoutAdmin from "@/components/template/LayoutAdmin"
 import useFetch from "@/hooks/useFetch"
 import { ApiResponse, DashboardStatsProps } from "@/types/data.types"
+import Each from "@/utils/each.utils"
 import { Breadcrumb } from "antd"
 import { MdArticle, MdFolder, MdOutlineArticle } from "react-icons/md"
 
@@ -11,14 +12,30 @@ const Page = () => {
     const { data, loading, error } = useFetch<ApiResponse<DashboardStatsProps>>("/api/dashboard-stats", "GET");
 
     if (loading) {
-        return <p>Loadin...</p>
+        return <p>Loading...</p>
     }
 
     if (error) {
         return <p>Error...</p>
     }
 
-    console.log(data);
+    const dashboardCards = [
+        { 
+            title: "Jumlah Published", 
+            value: data?.data.published || 0, 
+            icon: <MdArticle /> 
+        },
+        { 
+            title: "Jumlah Draft",
+            value: data?.data.draft || 0, 
+            icon: <MdArticle /> 
+        },
+        { 
+            title: "Jumlah Category", 
+            value: data?.data.category || 0, 
+            icon: <MdFolder /> 
+        }
+    ];
 
     return (
         <LayoutAdmin>
@@ -31,33 +48,23 @@ const Page = () => {
                 ]}
             />
             <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
-                <div className="h-[150px] w-full bg-white shadow flex flex-col justify-center gap-4 items-center p-5 rounded-lg">
-                    <div className="h-12 w-12 text-white text-xl rounded-full bg-black flex items-center justify-center">
-                        <MdArticle  />
-                    </div>
-                    <div className="flex flex-col items-center  ">
-                        <h1 className="text-sm">Jumlah Published</h1>
-                        <h1 className="text-sm font-semibold">{data?.data.published ? data.data.published : 0}</h1>
-                    </div>
-                </div>
-                <div className="h-[150px] w-full bg-white shadow flex flex-col justify-center gap-4 items-center p-5 rounded-lg">
-                    <div className="h-12 w-12 text-white text-xl rounded-full bg-black flex items-center justify-center">
-                        <MdArticle  />
-                    </div>
-                    <div className="flex flex-col items-center  ">
-                        <h1 className="text-sm">Jumlah Draft</h1>
-                        <h1 className="text-sm font-semibold">0</h1>
-                    </div>
-                </div>
-                <div className="h-[150px] w-full bg-white shadow flex flex-col justify-center gap-4 items-center p-5 rounded-lg">
-                    <div className="h-12 w-12 text-white text-xl rounded-full bg-black flex items-center justify-center">
-                        <MdFolder  />
-                    </div>
-                    <div className="flex flex-col items-center  ">
-                        <h1 className="text-sm">Jumlah Category</h1>
-                        <h1 className="text-sm font-semibold">0</h1>
-                    </div>
-                </div>
+                <Each 
+                    of={dashboardCards}
+                    render={(items, i) => (
+                        <div 
+                            key={i} 
+                            className="h-[150px] w-full bg-white shadow flex flex-col justify-center gap-4 items-center p-5 rounded-lg"
+                        >
+                            <div className="h-12 w-12 text-white text-xl rounded-full bg-black flex items-center justify-center">
+                                {items.icon}
+                            </div>
+                            <div className="flex flex-col items-center  ">
+                                <h1 className="text-sm">{items.title}</h1>
+                                <h1 className="text-sm font-semibold">{items.value}</h1>
+                            </div>
+                        </div>
+                    )}
+                />
             </div>
         </LayoutAdmin>
     )
